717b65e4e2e4e4a9c7a8ddb9d0d83e1e4cd6a777
hailneed <hqclox43@gmail.com>
2026-09-14 22:36:38 +0300

    examples: add basic memory workflow

---
+++ examples/basic.py
@@ -0,0 +1,11 @@
+from engramscope import EngramScope
+
+lens = EngramScope("engramscope-demo.db")
+
+first, _ = lens.remember(key="project.database", value="PostgreSQL", source="conversation#1")
+second, conflict = lens.remember(key="project.database", value="MongoDB", source="conversation#9")
+
+print("current:", second.value)
+print("conflict:", bool(conflict))
+print("timeline:", [m.value for m in lens.timeline("project.database")])
+print("recall:", [(h.memory.value, h.score) for h in lens.recall("project database")])
